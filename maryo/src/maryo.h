25838771e8cdc16257dc1dd6a55ee8eea02303d8
maryorappr <sulaymanasif@gmail.com>
2022-07-30 19:24:51 -0700

    oops forgot to save

--- maryo/src/maryo.h
+++ maryo/src/maryo.h
@@ -3,8 +3,6 @@
 //FOR use by maryo applications
 
 #include "maryo\Application.h"
-#include <stdio.h>
-
 
 // ---Entry Point------------------------
 #include "maryo/EntryPoint.h"
--- maryo/src/maryo/EntryPoint.h
+++ maryo/src/maryo/EntryPoint.h
@@ -6,7 +6,6 @@ extern maryo::Application* maryo::CreateApplication();
 
 int main(int argc, char** argv)
 {
-	printf("maryo engine\n");
 	auto app = maryo::CreateApplication();
 	app->Run();
 	delete app;
